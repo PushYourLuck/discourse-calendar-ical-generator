29f0e316678ba536b7be8bf26fb0746e31663787
Karthik Balakrishnan <karthikb351@gmail.com>
2025-03-02 23:33:59 +0530

    Use UC google calendar feed

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export default {
 				const html = `<!DOCTYPE html>
 				<body>
 				<h2>Underline Center Calendar</h2>
-				<a href="https://www.google.com/calendar/render?cid=webcal%3A%2F%2Ffeed.underline.center%2Fcalendar.ics"><button>Subscribe via Google Calendar</button></a>
+				<a href="https://www.google.com/calendar/render?cid=64jp4d84mrqgphnpq7qkiq3c0jg9sapd%40import.calendar.google.com"><button>Subscribe via Google Calendar</button></a>
 				<p>If the link doesn't work automatically, you manually import the below the URL into your calendar app.<pre>https://feed.underline.center/calendar.ics</pre></p>
 				</body>`;
 
