1fa571e175b383927c87407fe0f5ce6a5aa0203a
Karthik Balakrishnan <karthikb351@gmail.com>
2025-04-28 16:32:22 +0000

    support of recurring events use `id`

--- src/index.ts
+++ src/index.ts
@@ -46,15 +46,17 @@ export default {
 					const startTime = new Date(event.starts_at);
 					const endTime = new Date(event.ends_at);
 
-					const ticketsLink = event.url ? `Tickets available at ${event.url}` : `Tickets coming soon'`;
+					const ticketsLink = event.url && event.url == 'tba' ? `Tickets available at ${event.url}` : `Tickets coming soon'`;
 					const description = `Details: https://underline.center${event.post.url}
 
 ${ticketsLink}`;
 					calendar.createEvent({
+						id: event.id,
 						start: startTime,
 						end: endTime,
 						summary: event.post.topic.title,
 						description: description,
+						repeating: event.recurrence_rule,
 						location: 'Underline Center, Indiranagar',
 						url: `https://underline.center${event.post.url}`,
 					});
